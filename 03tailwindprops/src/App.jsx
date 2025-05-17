60885807266fe1ab8122c876408621c2467ed8a8
aniketdhurwey245@gmail.com <aniketdhurwey245@gmail.com>
2025-05-17 13:23:51 +0530

    hiow to use tailwindcss and props

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -2,32 +2,23 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/Card'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  let myObj = {
+    userid:"aniket",
+    age:23
+
+  }
+
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1 className="p-1 shadow-xl bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-2xl mb-2" >Tailwind Test</h1>
+      <Card plainText='Science Of Conding And Development' someObj="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, adipisci."/>
+      {/* in card cannot pass directly array or object we fist define then pass variable */}
+      <Card plainText="Coding And Development With Vite+React" someObj={myObj}/>
     </>
   )
 }
